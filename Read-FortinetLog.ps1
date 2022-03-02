b16f141f7e0b3249d75b98948244394143728e3f
Ben Van den Bergh <ben.vandenbergh@gmail.com>
2022-03-02 09:14:13 +0100

    initial build

---
+++ Read-FortinetLog.ps1
@@ -0,0 +1,15 @@
+Function Read-FortinetLog {
+    [CmdletBinding()]
+    param(
+    [Parameter(Mandatory=$true)]$FilePath
+    )
+
+    Get-Content -Path $FilePath -Head 1 | ForEach-Object {
+        $obj = [PSCustomObject]@{}
+        ($_ -replace ".$").Substring(1).Split(""",""") | ForEach-Object {
+            "XX $_ XX"
+            $obj | Add-Member -MemberType NoteProperty -Name $_.Split("=")[0] -Value $_.Split("=",2)[1].replace("""","")
+        }
+        $obj
+    }
+}

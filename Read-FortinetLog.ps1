1d02e582436f6bfce969c1fe981f4580a40cfcd8
Ben Van den Bergh <ben.vandenbergh@gmail.com>
2022-03-02 13:24:42 +0100

    removed trblsht-line

--- Read-FortinetLog.ps1
+++ Read-FortinetLog.ps1
@@ -7,7 +7,6 @@ Function Read-FortinetLog {
     Get-Content -Path $FilePath | ForEach-Object {
         $obj = [PSCustomObject]@{}
         ($_ -replace ".$").Substring(1).Split(""",""") | ForEach-Object {
-            "XX $_ XX"
             $obj | Add-Member -MemberType NoteProperty -Name $_.Split("=")[0] -Value $_.Split("=",2)[1].replace("""","")
         }
         $obj

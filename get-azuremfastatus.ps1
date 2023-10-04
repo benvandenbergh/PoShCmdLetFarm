4eecbbbce0d4ce7fc0b800c452e7786db2d39608
Ben Van den Bergh <ben.vandenbergh@gmail.com>
2023-10-04 11:52:59 +0200

    return "other MFAmethods" as a string

--- get-azuremfastatus.ps1
+++ get-azuremfastatus.ps1
@@ -3,5 +3,5 @@ function get-AzureMFAstatus {
     Param (
         [Parameter(Mandatory=$true)][string]$UserPrincipalName
     )
-    Get-MsolUser -UserPrincipalName $userprincipalname | Select-Object userprincipalname,@{n="MFA Status";e={$_.StrongAuthenticationRequirements.state}},@{n="default MFAmethod";e={($_.StrongAuthenticationMethods | Where-Object IsDefault).MethodType}},@{n="other MFAmethods";e={($_.StrongAuthenticationMethods | Where-Object {!$_.IsDefault}).MethodType | Sort-Object}},@{n="PhoneNumber";e={$_.StrongAuthenticationUserDetails.PhoneNumber}},@{n="AlternativePhoneNumber";e={$_.StrongAuthenticationUserDetails.AlternativePhoneNumber}}
+    Get-MsolUser -UserPrincipalName $userprincipalname | Select-Object userprincipalname,@{n="MFA Status";e={$_.StrongAuthenticationRequirements.state}},@{n="default MFAmethod";e={($_.StrongAuthenticationMethods | Where-Object IsDefault).MethodType}},@{n="other MFAmethods";e={($_.StrongAuthenticationMethods | Where-Object {!$_.IsDefault}).MethodType -join ","}},@{n="PhoneNumber";e={$_.StrongAuthenticationUserDetails.PhoneNumber}},@{n="AlternativePhoneNumber";e={$_.StrongAuthenticationUserDetails.AlternativePhoneNumber}}
 }
